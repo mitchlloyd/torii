70a6449e9b50973de23564c26e4d92fc631f05fe
Cory Forsyth <cory.forsyth@gmail.com>
2015-10-12 16:56:46 -0400

    Add ember-try for emer 1.13.7, 2.0, 2.1, beta, canary

---
+++ config/ember-try.js
@@ -0,0 +1,41 @@
+/* jshint node:true */
+module.exports = {
+  scenarios: [
+    {
+      name: 'Ember 1.13.7',
+      dependencies: {
+        'ember': '1.13.7'
+      }
+    },
+    {
+      name: 'Ember 2.0.2',
+      dependencies: {
+        'ember': '2.0.2'
+      }
+    },
+    {
+      name: 'Ember 2.1.0',
+      dependencies: {
+        'ember': '2.1.0'
+      }
+    },
+    {
+      name: 'Ember Canary',
+      dependencies: {
+        'ember': 'components/ember#canary'
+      },
+      resolutions: {
+        'ember': 'canary'
+      }
+    },
+    {
+      name: 'Ember Beta',
+      dependencies: {
+        'ember': 'components/ember#beta'
+      },
+      resolutions: {
+        'ember': 'beta'
+      }
+    }
+  ]
+};
